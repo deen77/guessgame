3951780ab33f9d68388f7312477d14a08458d996
user <alithegreat1590@gmail.com>
2017-04-03 03:47:31 +0800

    Adding new set of wishes

--- guessgame.py
+++ guessgame.py
@@ -3,15 +3,16 @@ print "This is a guess game.\nYou just need to guess a number with given clue.\n
 name=raw_input("\nPlease enter player name: ")
 print "Welcome and I wish you good luck %s"%(name)
 print "You have only ten tries."
-guess_wish=["Let's guess a number", "Give me your number", "What's your lucky number?"]
-goodluck_wish=["Dont give up", "It's cold", "It's near", "You've gone too far mate!!", "Please try harder next time", "Do you have brain", "Almost near dude"]
+guess_wish=["Let's guess a number: ", "Give me your number: ", "What's your lucky number?: "]
+goodluck_wish=["Dont give up", "It's cold", "It's near", "Come on you can do it", "Please try harder!!", "Do you have brain?", "Almost dude"]
 lie=["yes", "no"]
 num=random.randrange(1,100)
 print num
 tries=0
 tries_remaining=10
 while tries < 10:
-    guess=random.choice(guess_wish)
+    guess = raw_input(random.choice(guess_wish))
+
     if tries < 5:
         if guess == num:
             print random.choice(lie)
