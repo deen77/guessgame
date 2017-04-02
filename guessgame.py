3c491ea5e7e2e8a6d5ba1a706cb6dc29fee67fd6
user <alithegreat1590@gmail.com>
2017-04-03 04:05:45 +0800

    Correcting the game's logik.

--- guessgame.py
+++ guessgame.py
@@ -3,29 +3,28 @@ print "This is a guess game.\nYou just need to guess a number with given clue.\n
 name=raw_input("\nPlease enter player name: ")
 print "Welcome and I wish you good luck %s"%(name)
 print "You have only ten tries."
-guess_wish=["Let's guess a number: ", "Give me your number: ", "What's your lucky number?: "]
-goodluck_wish=["Dont give up", "It's cold", "It's near", "Come on you can do it", "Please try harder!!", "Do you have brain?", "Almost dude"]
-lie=["yes", "no"]
-num=random.randrange(1,100)
+guess_wish = ["Let's guess a number: ", "Give me your number: ", "What's your lucky number?: "]
+goodluck_wish = ["Dont give up", "It's cold", "It's near", "Come on you can do it", "Please try harder!!", "Do you have brain?", "Almost dude"]
+lie = ["yes", "no", "yes", "no", "yes", "no"]
+num = random.randrange(1,100)
 print num
 tries=0
 tries_remaining=10
 while tries < 10:
-    guess = raw_input(random.choice(guess_wish))
+    guess = input(random.choice(guess_wish))
+    lie_status = random.choice(lie)
+    if guess == num:
+        print('Woww!! You made it!! The number is %d' % num)
+        break
+    elif guess > num:
+        if lie_status == 'no':
+            print('Your number is bigger. Please try again!')
+        else:
+            print('Your number is lower. Please try again!')
+    elif guess < num:
+        if lie_status == 'no':
+            print('Your number is lower. Please try again!')
+        else:
+            print('Your number is bigger. Please try again!')
 
-    if tries < 5:
-        if guess == num:
-            print random.choice(lie)
-        else:
-            print random.choice(lie)
-    elif tries == 5:
-        if guess == num:
-            print "You awesome %s .. i know u can do it"%(name)
-        else:
-            print "Please work harder %s"%(name)
-            print "The answer is %d"%(num)
-    else:
-        (tries == 5)
-        print "%s...your brain is not working mate!!!"%(name)
-        print "The answer is %d"%(num)
     tries += 1
